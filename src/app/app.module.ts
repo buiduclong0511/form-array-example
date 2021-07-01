ff87761a4478c8d7136d49921cd6e4f78443d51c
Longbd <buiduclong0511@gmail.com>
2021-07-01 16:09:17 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,16 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
-import { ProductModule } from './product/product.module';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ProductComponent } from './product/product.component';
-import { FormComponent } from './form/form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NotFoundComponent,
-    ProductComponent,
-    FormComponent,
     ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ProductModule,
-    AppRoutingModule,
     ReactiveFormsModule
   ],
   providers: [],
